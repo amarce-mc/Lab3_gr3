8c8c0c4a75984d39042bb18787d43ffe4f49ba9a
amarce-mc <115052876+amarce-mc@users.noreply.github.com>
2022-11-28 14:27:21 -0500

    Delete doc directory

--- doc/html/search/all_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['yellow',['yellow',['../main_8cpp.html#a34167711871149458e41ef5d3e3541fe',1,'main.cpp']]]
-];
--- doc/html/search/variables_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['cont',['cont',['../main_8cpp.html#a961800bf60ff693820efbf7f4bc72788',1,'main.cpp']]]
-];
